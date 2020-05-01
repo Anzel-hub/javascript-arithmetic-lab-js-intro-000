367c96dc5f031a92abb1a5778afd51f297cca6ee
Anzel <anzelnieuwenhuizen@gmail.com>
2020-05-01 17:32:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(){
+  var add = +1;
+  console.log(add);
+}
+
+function add(a, b){
+  var total = a + b;
+  return total;
+  console.log(total)
+}
+
+function subtract(a, b){
+  var total = a - b;
+  return total;
+  console.log(total)
+}
+
+function multiply(a, b){
+  var total = a * b;
+  return total;
+  console.log(total)
+}
+
+function divide(a, b){
+  var total = a / b;
+  return total;
+  console.log(total)
+}
+
+function increment(n){
+  var total = ++ n;
+  return total;
+  console.log(total)
+}
+
+function decrement(n){
+  var total = -- n;
+  return total;
+  console.log(total)
+}
+
+function makeInt(n){
+  var total = parseInt('hello', 10);
+  return total;
+}
